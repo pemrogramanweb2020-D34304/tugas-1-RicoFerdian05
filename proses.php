69dd191c24adde679b6d3bb490f1f92600a7dfa3
Rico Ferdian <71257965+RicoFerdian05@users.noreply.github.com>
2020-09-28 22:55:56 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,29 @@
+<?php
 
+    $bila1 = $_GET['bilangan1'];
+    $bila2 = $_GET['bilangan2'];
+    $oper = $_GET['operasi'];
+    $hasil = 0;
+    echo "Bilangan 1 = $bila1 <br>";
+    echo "Bilangan 2 = $bila2 <br>";
+
+    if($oper == "tambah" ){
+        $hasil = $bila1 + $bila2;
+        echo "Operasi hitung = tambah<br>$bila1 + $bila2 = $hasil";
+    }
+    elseif($oper == "kurang"){
+        $hasil = $bila1 - $bila2;
+        echo "Operasi hitung = kurang<br>$bila1 - $bila2 = $hasil";
+    }
+    elseif($oper == "kali"){
+        $hasil = $bila1 * $bila2;
+        echo "Operasi hitung = kali<br>$bila1 * $bila2 = $hasil";
+    }
+    elseif($oper == "bagi"){
+        $hasil = $bila1 / $bila2;
+        echo "Operasi hitung = bagi<br>>$bila1 / $bila2 = $hasil";
+    }
+    else{
+        echo "ERROR";
+    }
+?>
